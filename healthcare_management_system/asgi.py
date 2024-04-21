73802b7267bbeb0966d8a4580bb7d31a44d81a87
melcholign <115886450+melcholign@users.noreply.github.com>
2024-04-21 20:16:23 +0600

    Rename the project

--- healthcare_management_system/asgi.py
+++ healthcare_management_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hospitalManagemenmt project.
+ASGI config for healthcare_management_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospitalManagemenmt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthcare_management_system.settings')
 
 application = get_asgi_application()
