3128c1e57a94784b9482bf897d2c0d805e7660aa
melcholign <115886450+melcholign@users.noreply.github.com>
2024-04-21 21:14:12 +0600

    Make changes to models in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -3,28 +3,27 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Doctor(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    speciality = models.CharField(max_length=100)
+    specialty = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
     qualification = models.TextField()
-    email = models.EmailField(unique=True)
     contact = models.BigIntegerField()  # A ten digit number for mobile number
 
     def __str__(self):
-        return self.name + " - " + self.speciality
+        return "Dr. " + self.user.last_name + " - " + self.specialty
 
 
 class Patient(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    SEXES = (('m', 'male'), ('f', 'female'))
+    
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    dateOfBirth = models.DateField()
-    gender = models.CharField(max_length=10)  # Male or Female
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField(null=True, blank=True)
+    sex = models.CharField(choices=SEXES)  # Male or Female
     address = models.CharField(max_length=200)
     contact = models.BigIntegerField()  # A ten digit number for mobile number
-    email = models.EmailField()
 
     def __str__(self):
-        return self.name 
+        return self.user.first_name + ' ' + self.user.last_name
