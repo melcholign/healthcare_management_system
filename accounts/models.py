7dc5cd3f38e9bcb117cdcac63f196aad5db459fa
melcholign <115886450+melcholign@users.noreply.github.com>
2024-04-21 22:56:15 +0600

    Make some fields nullable in accounts models
    The user foreign keys are nullable so that patient and doctor data can
    be retained after user account deletion.
    The sex field is able to be null so that a default from the two choices
    do not have to be forced. otherwise, a default assignment would give
    the wrong information (it's better to have no information at all than
    to have bad information!)

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Doctor(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     id = models.AutoField(primary_key=True)
     specialty = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
@@ -17,11 +17,11 @@ class Doctor(models.Model):
 class Patient(models.Model):
     SEXES = (('m', 'male'), ('f', 'female'))
     
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     id = models.AutoField(primary_key=True)
     date_of_birth = models.DateField()
     date_of_death = models.DateField(null=True, blank=True)
-    sex = models.CharField(choices=SEXES)  # Male or Female
+    sex = models.CharField(null=True, choices=SEXES, max_length=1)  # Male or Female
     address = models.CharField(max_length=200)
     contact = models.BigIntegerField()  # A ten digit number for mobile number
 
