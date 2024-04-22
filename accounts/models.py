3a53f0f24e3573cb7548759e71c85cd0c7cd5d80
melcholign <115886450+melcholign@users.noreply.github.com>
2024-04-22 19:05:45 +0600

    Reset database and migrations

--- accounts/models.py
+++ accounts/models.py
@@ -34,5 +34,5 @@ class Institution(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(primary_key=True, max_length=200)
     
-def __str__(self):
+    def __str__(self):
         return self.name
