5c018060cd28f97634827a9a3ff34d9d6779b630
melcholign <115886450+melcholign@users.noreply.github.com>
2024-04-23 00:43:27 +0600

    Add workplace and error-handling to registerDoctor

--- accounts/views.py
+++ accounts/views.py
@@ -6,65 +6,79 @@ from datetime import date
 
 def registerDoctor(request):
     cursor = connection.cursor()
+    cursor.execute('SELECT name, address from accounts_institution')
+    institutions = cursor.fetchall()
+    
+    context = {
+        'workplaces': institutions,
+    }
+    
     if request.method == "POST":
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         specialty = request.POST['specialty']
         department = request.POST['department']
         qualification = request.POST['qualification']
+        workplace = request.POST['workplace']
         email = request.POST['email']
         contact = request.POST['contact']
         password = request.POST['password']
         
-        username = email[0:email.find('@')]
-        
-        # Create a new user instance for the doctor
-        userInsertQuery = '''INSERT INTO auth_user(password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
-             values (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        '''
-        cursor.execute(userInsertQuery, [password, False, username, first_name, last_name, email, False, True, date.today()])
+        cursor.execute(f'SELECT email from auth_user where email = \'{email}\'')
+        if cursor.fetchall():
+            context['error_message'] = 'Doctor with this email already exists'
+        else:    
+            # Create a new user instance for the doctor
+            username = email[0:email.find('@')]
+            userInsertQuery = '''INSERT INTO auth_user(username, email, password, first_name, last_name, date_joined, is_superuser, is_staff, is_active)
+                values (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            '''
+            cursor.execute(userInsertQuery, [username, email, password,  first_name, last_name, date.today(), False, False, True])
 
-        # Get the ID of the inserted user
-        id = cursor.lastrowid
+            # Get the ID of the inserted user
+            user_id = cursor.lastrowid
 
-        doctorInsertQuery = """
-            INSERT INTO accounts_doctor(id, specialty, department, qualification, contact) 
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-        """
-        cursor.execute(doctorInsertQuery, [id, specialty, department, qualification, contact])
-    return render(request, 'registerDoctor.html')
-
-
+            doctorInsertQuery = """
+                INSERT INTO accounts_doctor(user_id, specialty, department, qualification, workplace_id, contact, available) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(doctorInsertQuery, [user_id, specialty, department, qualification, workplace, contact, False])
+                    
+    return render(request, 'registerDoctor.html', context=context)
 
 def registerPatient(request):
     cursor = connection.cursor()
     if request.method == "POST":
-        name = request.POST['name']
-        dateOfBirth = request.POST['dateOfBirth']
-        gender = request.POST['gender']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        date_of_birth = request.POST['date_of_birth']
+        sex = request.POST['sex']
         address = request.POST['address']
-        phoneNumber = request.POST['phoneNumber']
+        contact = request.POST['contact']
         email = request.POST['email']
         password = request.POST['password']
 
         # Check if patient with the same email already exists
-        patientQuery = "SELECT * FROM accounts_patient WHERE email = %s"
+        patientQuery = "SELECT * FROM auth_user WHERE email = %s"
         cursor.execute(patientQuery, [email])
         results = cursor.fetchall()
         if len(results) > 0:
             return render(request, 'registerPatient.html', {'error': 'Patient with this email already exists'})
         else:
             # Create a new user instance for the patient
-            userInsertQuery = "INSERT INTO auth_user(username, password) VALUES (%s, %s)"
-            cursor.execute(userInsertQuery, [email, password])
+            username = email[0:email.find('@')]
+            userInsertQuery = '''INSERT INTO auth_user(password, is_superuser, username, first_name, last_name, email, is_staff, is_active, date_joined)
+             values (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            '''
+            cursor.execute(userInsertQuery, [password, False, username, first_name, last_name, email, False, True, date.today()])
             user_id = cursor.lastrowid
 
             # Insert data into accounts_patient table
             patientInsertQuery = """
-                INSERT INTO accounts_patient(user_id, name, dateOfBirth, gender, address, contact, email) 
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                INSERT INTO accounts_patient(user_id, date_of_birth, sex, address, contact) 
+                VALUES (%s, %s, %s, %s, %s)
             """
-            cursor.execute(patientInsertQuery, [user_id, name, dateOfBirth, gender, address, phoneNumber, email])
+            cursor.execute(patientInsertQuery, [user_id, date_of_birth, sex, address, contact])
 
     return render(request, 'registerPatient.html')
 
@@ -76,7 +90,7 @@ def login(request):
         cursor = connection.cursor()
 
         # Check if the user exists
-        userQuery = "SELECT * FROM auth_user WHERE username = %s AND password = %s"
+        userQuery = "SELECT * FROM auth_user WHERE email = %s AND password = %s"
         cursor.execute(userQuery, [email, password])
         results = cursor.fetchall()
         if len(results) > 0:
