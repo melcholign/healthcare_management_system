d7974269dfbfc2d83a5ec22cb2aa9d494911459c
melcholign <115886450+melcholign@users.noreply.github.com>
2024-04-22 21:02:01 +0600

    Revamp the patient registration form

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,4 @@ from .import views
 urlpatterns = [
     path('registerDoctor',views.registerDoctor,name='registerDoctor'),
     path('registerPatient',views.registerPatient,name='registerPatient'),
-    
 ]
