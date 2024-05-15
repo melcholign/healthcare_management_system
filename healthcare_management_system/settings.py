d3d9d484c3f7e158ab70e01361ec9b6f72eb2bdc
melcholign <115886450+melcholign@users.noreply.github.com>
2024-05-16 02:08:55 +0600

    Add installation setting for drf app
    drf = django rest framework

--- healthcare_management_system/settings.py
+++ healthcare_management_system/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'corsheaders',
+    
+    # project-specific apps
     'accounts',
     'index',
 ]
